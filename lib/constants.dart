688f52d386c43fd7c0c1a8b45088d8391202c513
EnirysK <syrinekhelifi2198@gmail.com>
2020-12-20 22:19:37 +0100

    Created the home screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kBlueColor = Color(0xFF4668d9);
 const kRedColor = Color(0xFFfc5770);
 const kGreenColor = Color(0xFF07a581);
+const kBgColor = Color(0xFFf9fbfc);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,59 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBlueColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SearchBar(),
+            ),
+            Container(
+              color: kGreenColor,
+              child: Column(
+                children: [
+                  Card(
+                    child: ListTile(
+                      onTap: () {},
+                      title: Text('Hello world!'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: TextField(
+          decoration: InputDecoration(
+            icon: Icon(
+              Icons.search,
+              color: Colors.grey[400],
+            ),
+            hintText: "Search Properties, Areas etc ...",
+            border: InputBorder.none,
+          ),
+        ),
+      ),
     );
   }
 }
